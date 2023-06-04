954cea554562311e67afaf8b6c09de8872bae7ab
Jeppe Knudsen <jeppeknudsen1998@hotmail.com>
2023-06-04 17:56:07 +0200

    fixed some index issue

--- ClothingApplication/DAL/ClothContext.cs
+++ ClothingApplication/DAL/ClothContext.cs
@@ -31,10 +31,10 @@ namespace ClothingApplication.DAL
 
                 var getBrands = context.brand.ToList();
 
-                context.Pants.Add(new Pants { _brand = getBrands.ElementAt(1), _color = "Grey", _fabric = "Denim", _image = null, _inventory = 40, _price = 799, _size = "Medium", _waistSize = 38 });
-                context.Jacket.Add(new Jacket { _brand = getBrands.ElementAt(2), _color = "Black", _fabric = "Leather", _image = null, _inventory = 30, _price = 1100, _size = "Medium", _hasHood = false });
-                context.Jacket.Add(new Jacket { _brand = getBrands.ElementAt(3), _color = "Beige", _fabric = "Denim", _image = null, _inventory = 30, _price = 900, _size = "Medium", _hasHood = true });
-                context.T_Shirt.Add(new T_shirt { _brand = getBrands.ElementAt(4), _color = "Yellow", _fabric = "100% Cotton", _image = null, _inventory = 100, _price = 550, _size = "Small" });
+                context.Pants.Add(new Pants { _brand = getBrands.ElementAt(0), _color = "Grey", _fabric = "Denim", _image = null, _inventory = 40, _price = 799, _size = "Medium", _waistSize = 38 });
+                context.Jacket.Add(new Jacket { _brand = getBrands.ElementAt(1), _color = "Black", _fabric = "Leather", _image = null, _inventory = 30, _price = 1100, _size = "Medium", _hasHood = false });
+                context.Jacket.Add(new Jacket { _brand = getBrands.ElementAt(2), _color = "Beige", _fabric = "Denim", _image = null, _inventory = 30, _price = 900, _size = "Medium", _hasHood = true });
+                context.T_Shirt.Add(new T_shirt { _brand = getBrands.ElementAt(3), _color = "Yellow", _fabric = "100% Cotton", _image = null, _inventory = 100, _price = 550, _size = "Small" });
             }
         }
 
